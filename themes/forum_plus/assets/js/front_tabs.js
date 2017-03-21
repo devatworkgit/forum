dc3bd602479cdb7b30457399a25f4393b5d25c23
Ion <macaria.ion@gmail.com>
2017-03-21 10:22:51 +0200

    tebs

--- themes/forum_plus/assets/js/front_tabs.js
+++ themes/forum_plus/assets/js/front_tabs.js
@@ -2,13 +2,31 @@
   
   Drupal.behaviors.front_tabs = {
     attach: function (context) {
-      $('#block-fronttabs a[href="#forum"], #block-fronttabs a[href="#q&a"], #block-fronttabs a[href="#blogs"]').each( function(index) {
-        if ($(this).attr('href') == '#forum' && !$('#block-forum-plus-content').hasClass('hidden')) {
-          $(this).addClass('active');
-        } else if ($(this).attr('href') == '#q&a' && !$('#block-views-block-q-a-block-1').hasClass('hidden')) {
-          $(this).addClass('active');
-        } else if ($(this).attr('href') == '#blogs' && !$('#block-views-block-blogs-block-1').hasClass('hidden')) {
-          $(this).addClass('active');
+      
+      $('#block-forum-plus-content, #block-views-block-q-a-block-1, #block-views-block-blogs-block-1').addClass('hidden');
+      
+      $('#block-fronttabs .content li a').each( function(index) {
+        if ($(this).attr('href') == '#forum') {
+          if (index == 0) {
+            $('#block-forum-plus-content').removeClass('hidden');
+          }
+          if(!$('#block-forum-plus-content').hasClass('hidden')) {
+            $(this).addClass('active');
+          }
+        } else if ($(this).attr('href') == '#q&a') {
+          if (index == 0) {
+            $('#block-views-block-q-a-block-1').removeClass('hidden');
+          }
+          if(!$('#block-views-block-q-a-block-1').hasClass('hidden')) {
+            $(this).addClass('active');
+          }
+        } else if ($(this).attr('href') == '#blogs') {
+          if (index == 0) {
+            $('#block-views-block-blogs-block-1').removeClass('hidden');
+          }
+          if(!$('#block-views-block-blogs-block-1').hasClass('hidden')) {
+            $(this).addClass('active');
+          }
         }
       });
       $('#block-fronttabs a[href="#forum"], #block-fronttabs a[href="#q&a"], #block-fronttabs a[href="#blogs"]').click( function(event) {
