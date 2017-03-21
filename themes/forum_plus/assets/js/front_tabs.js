032188bd0396a347b77a10870a79e07c935f398a
Ion <macaria.ion@gmail.com>
2017-03-21 10:27:45 +0200

    debug

--- themes/forum_plus/assets/js/front_tabs.js
+++ themes/forum_plus/assets/js/front_tabs.js
@@ -3,9 +3,9 @@
   Drupal.behaviors.front_tabs = {
     attach: function (context) {
       
-      $('#block-forum-plus-content, #block-views-block-q-a-block-1, #block-views-block-blogs-block-1').addClass('hidden');
+      $('body.path-frontpage #block-forum-plus-content, body.path-frontpage #block-views-block-q-a-block-1, body.path-frontpage #block-views-block-blogs-block-1').addClass('hidden');
       
-      $('#block-fronttabs .content li a').each( function(index) {
+      $('#block-fronttabs a[href="#forum"], #block-fronttabs a[href="#q&a"], #block-fronttabs a[href="#blogs"]').each( function(index) {
         if ($(this).attr('href') == '#forum') {
           if (index == 0) {
             $('#block-forum-plus-content').removeClass('hidden');
