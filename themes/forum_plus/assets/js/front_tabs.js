8e625ca36cc8bac0a940c138503c2b3a370a7ef6
Ion <macaria.ion@gmail.com>
2017-03-24 12:02:39 +0200

    tabs

--- themes/forum_plus/assets/js/front_tabs.js
+++ themes/forum_plus/assets/js/front_tabs.js
@@ -5,43 +5,59 @@
       
       $('body.path-frontpage #block-forum-plus-content, body.path-frontpage #block-views-block-q-a-block-1, body.path-frontpage #block-views-block-blogs-block-1').addClass('hidden');
       
-      $('#block-fronttabs a[href="#forum"], #block-fronttabs a[href="#q&a"], #block-fronttabs a[href="#blogs"]').each( function(index) {
-        if ($(this).attr('href') == '#forum') {
-          if (index == 0) {
-            $('#block-forum-plus-content').removeClass('hidden');
+      if ($(location).attr('hash') == '' && $('#block-fronttabs').length) {
+        $('a[href="/#forum"], a[href="/#q&a"], a[href="/#blogs"]').each( function(index) {
+          if ($(this).attr('href') == '/#forum') {
+            if (index == 0) {
+              $('#block-forum-plus-content').removeClass('hidden');
+            }
+            if(!$('#block-forum-plus-content').hasClass('hidden')) {
+              $(this).addClass('active');
+            }
+          } else if ($(this).attr('href') == '/#q&a') {
+            if (index == 0) {
+              $('#block-views-block-q-a-block-1').removeClass('hidden');
+            }
+            if(!$('#block-views-block-q-a-block-1').hasClass('hidden')) {
+              $(this).addClass('active');
+            }
+          } else if ($(this).attr('href') == '/#blogs') {
+            if (index == 0) {
+              $('#block-views-block-blogs-block-1').removeClass('hidden');
+            }
+            if(!$('#block-views-block-blogs-block-1').hasClass('hidden')) {
+              $(this).addClass('active');
+            }
           }
-          if(!$('#block-forum-plus-content').hasClass('hidden')) {
-            $(this).addClass('active');
-          }
-        } else if ($(this).attr('href') == '#q&a') {
-          if (index == 0) {
-            $('#block-views-block-q-a-block-1').removeClass('hidden');
-          }
-          if(!$('#block-views-block-q-a-block-1').hasClass('hidden')) {
-            $(this).addClass('active');
-          }
-        } else if ($(this).attr('href') == '#blogs') {
-          if (index == 0) {
-            $('#block-views-block-blogs-block-1').removeClass('hidden');
-          }
-          if(!$('#block-views-block-blogs-block-1').hasClass('hidden')) {
-            $(this).addClass('active');
-          }
+        });
+      }
+      
+      if ($(location).attr('hash') == '#forum') {
+        $('#block-forum-plus-content').removeClass('hidden');
+        $('a[href="/#forum"]').addClass('active');
+      } else if ($(location).attr('hash') == '#q&a') {
+        $('#block-views-block-q-a-block-1').removeClass('hidden');
+        $('a[href="/#q&a"]').addClass('active');
+      } else if ($(location).attr('hash') == '#blogs') {
+        $('#block-views-block-blogs-block-1').removeClass('hidden');
+        $('a[href="/#q&a"]').addClass('active');
+      }
+      
+      $('a[href="/#forum"], a[href="/#q&a"], a[href="/#blogs"]').click( function(event) {
+        if ($('body').hasClass('front')) {
+          event.preventDefault();
         }
-      });
-      $('#block-fronttabs a[href="#forum"], #block-fronttabs a[href="#q&a"], #block-fronttabs a[href="#blogs"]').click( function(event) {
-        event.preventDefault();
-        $('#block-fronttabs a[href="#forum"], #block-fronttabs a[href="#q&a"], #block-fronttabs a[href="#blogs"]').removeClass('active');
+        $('a[href="/#forum"], a[href="/#q&a"], a[href="/#blogs"]').removeClass('active');
         $('#block-forum-plus-content, #block-views-block-q-a-block-1, #block-views-block-blogs-block-1').addClass('hidden');
-        if ($(this).attr('href') == '#forum') {
+        if ($(this).attr('href') == '/#forum') {
           $('#block-forum-plus-content').removeClass('hidden');
-          $(this).addClass('active');
-        } else if ($(this).attr('href') == '#q&a') {
+          $('a[href="' + $(this).attr('href') + '"]').addClass('active');
+        } else if ($(this).attr('href') == '/#q&a') {
           $('#block-views-block-q-a-block-1').removeClass('hidden');
-          $(this).addClass('active');
-        } else if ($(this).attr('href') == '#blogs') {
+          $('a[href="' + $(this).attr('href') + '"]').addClass('active');
+        } else if ($(this).attr('href') == '/#blogs') {
           $('#block-views-block-blogs-block-1').removeClass('hidden');
-          $(this).addClass('active');
+          $('a[href="' + $(this).attr('href') + '"]').addClass('active');
         }
       })
     }
